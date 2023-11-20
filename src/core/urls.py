814df95de0e15fa1151018e91ced137a72779026
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-11-20 13:34:45 +0330

    feat: add chat urls and routing

--- src/core/urls.py
+++ src/core/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('auths.urls')),
+    path('accounts/', include('auths.urls')),
+    path('chat/', include('chat.urls')),
 ]
