bb4a68d277b3f5f08d21add0c55dbd1e5b16a5ca
Abolfazl zaker <abolfazlzaker.dev@gmail.com>
2023-11-20 13:10:12 +0330

    feat: add 404 template

--- src/auths/views.py
+++ src/auths/views.py
@@ -8,14 +8,15 @@ def register(request):
     if request.method == 'POST':
         form  = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
+            password = form.cleaned_data['password2']
 
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
                 return redirect('chat:index')
-            return redirect('login')
+            return redirect('auths:login')
     else:
         form = UserRegisterForm()
     return render(request, 'auths/register.html', {'form': form})
